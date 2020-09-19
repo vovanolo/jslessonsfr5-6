701587a149f9addca932bdef7e5f54bdac7f00d2
vovanolo <vovchik.student221@gmail.com>
2020-09-19 17:48:26 +0300

    lesson1

---
+++ lesson1/index.js
@@ -0,0 +1,140 @@
+// console.log("Hello world!");
+// let var const
+
+// let message = 'Hello world';
+// let age = 15;
+// console.log(age);
+// age = 16;
+// console.log(age);
+
+
+// const age = 15;
+// console.log(age);
+// age = 16;
+// console.log(age);
+
+// let name;
+// console.log(name);
+
+// console.log(home);
+
+// типи змінних
+
+// числові
+// const number = 20;
+
+// стрічкові
+// const message = 'hello world';
+
+// булеві або логічні
+// const a = true;
+// const b = false;
+
+// null
+// const some = null;
+// console.log(some);
+
+
+// undefined
+// let a;
+// console.log(a);
+
+
+// type of
+
+// let a;
+// console.log(typeof a);
+
+// let b = null;
+// console.log(typeof b);
+
+// let c = 5;
+// console.log(typeof c);
+
+// let d = 'js is good';
+// console.log(typeof d);
+
+// let e = false;
+// console.log(typeof e);
+
+
+
+// console.log() alert() prompt()
+// alert("Hello world!");
+// let message = prompt('введіть повідомлення');
+// console.log(message);
+
+// математичні оператори
+// const x = 10;
+// const y = 5;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// let number = 10;
+// number += 20;
+// number = number + 10;
+// console.log(number);
+
+
+// Порівняння
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x<z);
+// console.log(x<=z);
+// console.log(x===y);
+// console.log(x===z);
+// console.log(x!==y);
+
+
+// числа
+
+// const number = '5';
+// // Number(number);
+// console.log(typeof Number(number));
+
+// const number = '5';
+// console.log(typeof parseInt(number));
+
+
+// обєкт Math
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.min(40,20,30,50,10,60));
+// console.log(Math.max(40,20,30,50,10,60));
+
+// console.log(Math.random());//0-1
+// console.log(Math.random()*10);
+
+
+// Повідомлення або стрічки
+
+// const message = 'vova '+'is '+'happy';
+// console.log(message);
+
+// const name = 'ivan';
+// const age = 2;
+// const mood = 'happy';
+
+// const message = 'hello im '+name+' im '+age+' year old'+' and im '+ mood; 
+// const message = `hello im ${name} im ${age} years old and im ${mood}`;
+// console.log(message);
+
+
+
+
+
+
+
+
+
+
+
+
