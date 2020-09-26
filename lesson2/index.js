cb82e71a270a06b1420c39d004f6dc75e752d165
vovanolo <vovchik.student221@gmail.com>
2020-09-26 17:16:41 +0300

    lesson2

---
+++ lesson2/index.js
@@ -0,0 +1,80 @@
+//let const
+
+// console.log() alert() prompt()
+
+// const name = "Генератор захисного поля";
+// let price = 1000;
+// console.log(`Обрано «${name}», ціна за штуку ${price} кредитів`);
+// price = 2000;
+// console.log(`Обрано «${name}», ціна за штуку ${price} кредитів`);
+// console.log(Math.round(Math.random() * 1000));
+
+// let message = "Welcome";
+// let info = message.toUpperCase();
+// let info = message.toLowerCase();
+
+// console.log(message.length);
+// 1,2,3,4
+// 0,1,2,3,4
+
+// w-1 e-2 l-3 .....
+// w-0 e-1 l-2 c-3 o-4
+// console.log(message.indexOf("m"));
+// console.log(message.includes("m"));
+
+// Оператори умови
+// if else
+
+// if(умова){
+//   команда
+// }
+
+// else if
+
+// let price = 0;
+
+// const player = "looser";
+
+// if (player === "pro") {
+//   price = 100;
+// } else {
+//   price = 50;
+// }
+
+// if (player === "pro") {
+//   price = 100;
+// } else if (player === "middle") {
+//   price = 70;
+// } else if (player === "beginer") {
+//   price = 60;
+// } else {
+//   price = 50;
+// }
+
+// console.log(price);
+
+// let age = 21;
+
+// let result = (умова) ? результат коли виконується : результат коли не виконується
+// let result = age < 18 ? "Ви не повнолітній" : "Ви повнолітній";
+// console.log(result);
+
+// Оператор switch
+
+// let country = "таджикистан";
+
+// switch (country) {
+//   case "китай":
+//     console.log("Привіт по китайськи");
+//     break;
+//   case "індія":
+//     console.log("Привіт по індійськи");
+//     break;
+//   case "німеччина":
+//     console.log("Привіт по німецьки");
+//     break;
+
+//   default:
+//     console.log("Я такої країни не знаю");
+//     break;
+// }
