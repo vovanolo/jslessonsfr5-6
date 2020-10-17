66ec21352f23984563fc6eb14c4edd2f48c50e2b
vovanolo <vovchik.student221@gmail.com>
2020-10-17 17:58:58 +0300

    lesson5

---
+++ lesson5/index.js
@@ -0,0 +1,109 @@
+// console.log("hello");
+
+// розбір домашнього номер 6
+
+// let total = 0;
+// let canInput = true;
+
+// while (canInput) {
+//   const userInput = prompt("Введіть число");
+//   if (userInput === null) {
+//     canInput = false;
+//     console.log(`Total: ${total}`);
+//   } else {
+//     const numberToAdd = parseInt(userInput);
+//     total += numberToAdd;
+//   }
+// }
+
+// const name = "ivan";
+// const age = 16;
+// const mood = "happy";
+
+// console.log(`my name is ${name} i am ${age} years old and im ${mood}`);
+
+// "1" = 1
+
+// повторення
+// if (1) {
+//   console.log("hello");
+// }
+// while(0){
+
+// }
+
+// let i = 3;
+
+// while (i) {
+//   console.log(i--);
+// }
+
+// задачка
+// const number = +prompt("Введіть число");
+
+// if (number === 0) {
+//   console.log("це нуль");
+// } else if (number === 1) {
+//   console.log("це одиниця");
+// } else {
+//   console.log("це не нуль і не одиниця");
+// }
+
+// default:
+
+// switch
+
+// switch (number) {
+//   case 0:
+//     console.log("Це нуль");
+//     break;
+//   case 1:
+//     console.log("Це одиниця ");
+//     break;
+//   default:
+//     console.log("це не нуль і не один");
+//     break;
+// }
+
+// let fruits = ["яблука", "груші", "апельсини"];
+// let cart = fruits;
+// cart.push("банани");
+// console.log(cart.length);
+
+// задача
+// 1.створити масив styles з елементами джаз і блюз
+// 2.добавити в кінець масиву елемент рок-н-рол
+// 3.замінити елемент блюз на елемент класика
+// 4.видалити перший елемент масиву
+// 5.вставити елемент реп в початок масиву
+// let styles = ["джаз", "блюз"];
+// styles.push("рок-н-ролл");
+// // styles[1] = "класика";
+// styles.splice(1, 1, "класика");
+// styles.shift();
+// styles.unshift("реп");
+// console.log(styles);
+
+// функції
+// const showMessage = function () {
+//   console.log("hello world");
+// };
+// showMessage();
+
+// const sum = function (a, b) {
+//   console.log(a + b);
+// };
+
+// sum(10, 20);
+
+// const devide = function (x, y) {
+//   console.log(x / y);
+// };
+// devide(100, 20);
+
+// const multiply = function (a, b, c) {
+//   console.log(a * b * c);
+// };
+// multiply(20, 2, 4);
+// дз модуль 2 завдання 1
+// https://github.com/goitacademy/javascript-homework/blob/master/homework-02/README.ua.md
